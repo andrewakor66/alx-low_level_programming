614268f6acee74e2df69317ced7e11a8ecb586cf
andrewakor66 <andrewakor66@gmail.com>
2022-12-18 02:57:38 -0800

    task0

--- alx-low_level_programming/0-isupper.c
+++ alx-low_level_programming/0-isupper.c
@@ -4,13 +4,15 @@
  * _isupper - upipercase letters
  * @c: char to check
  *
- * Return: 0 or 1
+ * Return: 1 for upper letter or 0 for any else
  */
 
 int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+	{
+	return (1);
+	}
+	return (0);
 }
