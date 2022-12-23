ba7edaa51ef33f6bffeb78e0a01a9472d0d56a00
andrewakor66 <andrewakor66@gmail.com>
2022-12-23 04:17:24 -0800

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,10 @@
 
 void print_alphabet(void)
 {
-	int j;
+	char letter;
 
-	for (j = 'a'; j <= 'z'; j++)
-	{
-		_putchar(j);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
 }
