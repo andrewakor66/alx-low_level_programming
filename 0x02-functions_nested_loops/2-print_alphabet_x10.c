30e4f30785050ad95f484db2bf2442b6f2a9666c
andrewakor66 <andrewakor66@gmail.com>
2022-12-23 23:33:11 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,13 @@
 
 void print_alphabet_x10(void)
 {
+	int count = 0;
 	char letter;
+
+	while (count++ <= 9)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	_putchar('\n')
+		_putchar('\n')
 	}
 }
